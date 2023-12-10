13f531dae47a1cf9ec702e727f4266afdae88620
jackcausse <couriel650@gmail.com>
2023-12-10 12:10:22 +0100

    modification dossier image

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,5 +1,3 @@
-// Importation de fs
-const fs = require('fs')
 // Importation de multer
 const multer = require('multer')
 
@@ -10,31 +8,22 @@ const MIME_TYPES = {
   'image/png': 'png',
 }
 
-try {
-  const ok = fs.opendirSync('images')
-  // console.log(ok)
-} catch (e) {
-  fs.mkdirSync('images')
-}
-
 // Gère le fichier image
 const storage = multer.diskStorage({
+
   // indique à multer la destination du dossier d'enregistrement
   destination: (req, file, callback) => {
-    // console .log(444)
     callback(null, 'images')
-        // console.log('555')
-
   },
   // Indique à multer quel nom de fichier à utiliser
   filename: (req, file, callback) => {
-    // console.log('111')
-    // remplace les espaces par des underscores pour eviter des probkème côté serveur
+  
+    // remplace les espaces par des underscores pour eviter des problèmes côté serveur
     const name = file.originalname.split(' ').join('_')
     const extension = MIME_TYPES[file.mimetype]
     // Ajoute un timestamp
     callback(null, name + Date.now() + '.' + extension)
-    // console.log('33')
+  
   },
 })
 
